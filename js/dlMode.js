3ff4ac5bde2bc5e0a230449ff9ee83b1ea080b76
lazyVolume <jme71143@gmail.com>
2025-05-15 19:03:20 +0800

    Add files via upload

--- js/dlMode.js
+++ js/dlMode.js
@@ -25,3 +25,12 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const closeBtn = document.getElementById("closeBtn");
+  const popup = document.getElementById("popup");
+
+  closeBtn.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+});
+
